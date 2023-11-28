a1d2d0daa70e0c94e6c974edcf11a6cdf62c375d
Bennie Sowah Jr <bensowahjr@gmail.com>
2023-11-28 20:50:30 +0000

    testing

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, KING! From Green Team'
+    return 'Hello, ALEX! From RED Team'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
